343c0b28c40183e72a9cc08f8f2b8dea7ce00b94
exadmin <exadmin@mail.ru>
2022-05-27 21:03:04 +0300

    saveimage method is added

--- src/main/java/com/github/exadmin/opencv4j/FileUtils.java
+++ src/main/java/com/github/exadmin/opencv4j/FileUtils.java
@@ -1,7 +1,13 @@
 package com.github.exadmin.opencv4j;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class FileUtils {
 
@@ -12,5 +18,15 @@ public class FileUtils {
      */
     public static Mat loadImageFileToMat(String fileName) {
         return Imgcodecs.imread(fileName);
+    }
+
+    public static boolean saveImageToFile(Image fxImage, String fileName) {
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(fxImage, null), "png", new File(fileName));
+            return true;
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+            return false;
+        }
     }
 }
